92b1c118c28476bbdbbf8af2a373356472c660ab
FireNeslo <fireneslo@gmail.com>
2018-01-28 23:18:38 +0100

    torrent-site

--- client.js
+++ client.js
@@ -1,136 +1,176 @@
-import { idbm, a } from './client/utils.js'
+import { idbm, a, p, update, create } from './client/utils.js'
 
 const uri = new URL(location.href)
-const client = new WebTorrent({})
+const client = new WebTorrent()
 const sites = idbm('sites')
 
-if(location.hash.length > 1) {
-  install(location.hash.slice(1)).then(console.log)
+const parser = new DOMParser()
+
+const trackers = {
+  announce: [
+    'wss://tracker.openwebtorrent.com',
+    'wss://tracker.btorrent.xyz',
+    'wss://tracker.fastcast.nz'
+  ]
 }
 
-function tag(tag, items=[], attrs={}) {
-  const row = document.createElement(tag)
-
-  for(const item of items) {
-    row.appendChild(item)
-  }
-
-  for(const [attr, value] of Object.entries(attrs)) {
-    row.setAttribute(attr, value)
-  }
-
-  return row
-}
-
-function row(columns) {
-  return tag('tr', columns)
-}
-function col(textContent) {
-  return tag('td', [ text(textContent) ])
-}
-
-function text(text) {
-  return document.createTextNode(text)
-}
-
-sites.entries().then(items => {
-  const rows = items.map(([manifest, href]) => {
-    return row([
-      col(manifest.name),
-      tag('td', [ tag('a', [ text(href) ], { href }) ])
-    ])
-  })
-
-  const table = tag('table', [
-    tag('thead', [
-      row([ col('name'), col('location') ])
-    ]),
-    tag('tbody', rows)
-  ])
-  document.body.appendChild(table)
+update(document.body, {
+  hash: '',
+  trackers: trackers.announce.join('\n')
 })
 
+function appLink(manifest) {
+  const url = new URL(location.href)
+  const alias = (manifest.short_name || manifest.name).toLowerCase()
 
-const form = document.querySelector('form')
+  url.hostname = [ alias, location.hostname ].join('.')
 
-form.addEventListener('submit', event => {
-  const input = document.querySelector('[name=hash]')
-  event.preventDefault()
-  install(input.value)
-})
+  return url.origin
+}
 
-function install(id) {
-  console.log('installing' ,id)
-  return new Promise(resolve => {
-    const trackers = {
-      announce: [
-        'wss://tracker.openwebtorrent.com',
-        'wss://tracker.btorrent.xyz',
-        'wss://tracker.fastcast.nz'
-      ]
+renderApps()
+
+function iconPicture(manifest) {
+  const base = appLink(manifest)
+  const picture = document.createElement('iframe')
+  picture.setAttribute('scrolling', 'no')
+  const icon = manifest.icons.pop() || {}
+  const iconLink = [base, icon.src].join('/')
+
+  picture.src = iconLink
+
+  return picture
+}
+
+function renderApp({ manifest, location, pending }) {
+  const existing = document.getElementById(manifest.hash)
+  const { name, icons, hash } = manifest
+
+  const icon = pending
+    ? document.createElement('progress')
+    : iconPicture(manifest)
+
+  icon.setAttribute('html', 'icon')
+
+  if(existing) {
+    return update(existing, { name, icon, location, hash })
+  }
+
+  return create('#app', { name, icon, location, hash })
+}
+
+function renderApps() {
+  sites.entries().then(entries => {
+    const apps = document.querySelector('#apps')
+
+    for(const [ manifest, location ] of entries) {
+      const app = renderApp({ manifest, location })
+      if(!app.parentNode) {
+        apps.appendChild(app)
+      }
     }
-
-    client.add(id, trackers, async function (torrent) {
-      console.log('resolved', torrent)
-
-      const cache = await caches.open(torrent.infoHash)
-      console.log('waiting')
-
-      await new Promise(resolve => torrent.on('done', resolve))
-
-      console.log('done downloading')
-
-      for(const file of torrent.files) {
-        const request = await a(cb => file.getBlob(cb))
-
-        await cache.put(file.path, new Response(request))
-      }
-      const index = await cache.match('index.html')
-
-      const parser = new DOMParser()
-
-      const document = parser.parseFromString(await index.text(), 'text/html')
-
-      const link = document.querySelector('[rel="manifest"]')
-      const manifestFile = link && await cache.match(link.getAttribute('href'))
-      const manifest = manifestFile
-        ? await manifestFile.json()
-        : { name: document.querySelector('title').textContent }
-
-      manifest.hash = torrent.infoHash
-
-      const installer = document.createElement('iframe')
-      const host = new URL(location.origin)
-
-      host.hostname = [ manifest.short_name, location.hostname ].join('.')
-      host.pathname = '/install.html'
-
-      installer.setAttribute('src', host.href)
-
-      window.document.body.appendChild(installer)
-
-      await new Promise(resolve => installer.onload = resolve)
-
-      installer.contentWindow.postMessage({ manifest }, '*')
-
-      const paths = await cache.keys()
-
-
-      for(const request of paths) {
-        const response = await cache.match(request)
-        const contents = await response.arrayBuffer()
-        const url = new URL(request.url)
-        const file = { path: url.pathname, contents }
-
-        installer.contentWindow.postMessage({ file }, '*')
-      }
-
-      installer.contentWindow.postMessage({ done: true }, '*')
-
-
-      sites.set(host.origin, manifest)
-
-      resolve({ torrent, document, manifest })
-    })
   })
+}
+
+function res(promise, type='text') {
+  return promise.then(response => {
+    return new Response(response)[type]()
+  })
+}
+
+function file(tree, name, type) {
+  const file = tree[name.replace(/^\//, '')]
+  return res(a(cb => file.getBlob(cb)), type)
+}
+
+const $install = document.querySelector('.install')
+
+$install.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const hash = document.querySelector('[name=hash]')
+
+  install(hash.value)
+})
+
+function addToTree(tree, file) {
+  tree[file.path] = file
+  return tree
+}
+
+async function install(id) {
+  const apps = window.document.querySelector('#apps')
+
+  apps.classList.add('working')
+
+  const torrent = await p(cb => client.add(id, trackers, cb))
+
+  apps.classList.remove('working')
+
+  const app = renderApp({
+    manifest: { name: '?', hash: torrent.infoHash },
+    location: '#',
+    pending: true
+  })
+
+  if(!app.parentNode) {
+    apps.appendChild(app)
+  }
+
+  const cache = await caches.open(torrent.infoHash)
+
+  const tree = torrent.files.reduce(addToTree, {})
+
+  const index = await file(tree, 'index.html')
+  const document = parser.parseFromString(index, 'text/html')
+
+  const link = document.querySelector('[rel="manifest"]')
+
+  if(!link) throw new Error('Manifest required')
+
+  const manifest = await file(tree, link.getAttribute('href'), 'json')
+
+  manifest.hash = torrent.infoHash
+
+  const installer = document.createElement('iframe')
+  const origin = appLink(manifest)
+
+  renderApp({ manifest, location: origin, pending: true })
+
+  installer.setAttribute('src', [origin,'install.html'].join('/'))
+  installer.setAttribute('class', 'installer')
+
+  window.document.body.appendChild(installer)
+
+  const cached = torrent.files.map(async file => {
+    const blob = await a(cb => file.getBlob(cb))
+    const done = await cache.put(file.path, new Response(blob))
+    return done
+  })
+
+  await p((onload, onerror) => Object.assign(installer, { onload, onerror }))
+
+  installer.contentWindow.postMessage({ type: 'start', manifest }, '*')
+
+  await Promise.all(cached)
+
+  const paths = await cache.keys()
+
+  for(const request of paths) {
+    const response = await cache.match(request)
+    const contents = await response.arrayBuffer()
+    const url = new URL(request.url)
+    const path = url.pathname
+    const message = { type: 'file', contents, path, options: {} }
+
+    installer.contentWindow.postMessage(message, '*')
+  }
+
+  installer.contentWindow.postMessage({ type: 'complete' }, '*')
+
+  await p(cb => window.addEventListener('message', cb))
+
+  sites.set(origin, manifest)
+
+  renderApps()
 }
--- client/utils.js
+++ client/utils.js
@@ -5,6 +5,36 @@ export const IDBMap = idbm.IDBMap
 
 delete self.fnIdbm
 
+
+export function update(fragment, data) {
+  for(const input of fragment.querySelectorAll('[name]')) {
+    input.value = data[input.getAttribute('name')]
+  }
+  for(const text of fragment.querySelectorAll('[text]')) {
+    text.textContent = data[text.getAttribute('text')]
+  }
+  for(const node of fragment.querySelectorAll('[html]')) {
+    node.parentNode.replaceChild(data[node.getAttribute('html')], node)
+  }
+  for(const node of fragment.querySelectorAll('[set]')) {
+    const pairs = node.getAttribute('set').split(',')
+    for(const pair of pairs) {
+      const [attr, prop] = pair.split(':')
+      node.setAttribute(attr, data[prop])
+    }
+  }
+
+  return fragment
+}
+
+export function create(template, data) {
+  if(typeof template === 'string') {
+    template = document.querySelector(template)
+  }
+  return update(template.content.cloneNode(true), data)
+}
+
+
 export function a(cb) {
   return new Promise((resolve, reject) => {
     cb((error, result) => {
@@ -12,4 +42,8 @@ export function a(cb) {
       else return resolve(result)
     })
   })
+}
+
+export function p(cb) {
+  return new Promise(cb)
 }
